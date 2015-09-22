3ee6a3f2e7e1947bcc0e57c56dd76afbed097bb0
vitality <vitaly.kiktiov@gmail.com>
2015-09-22 17:19:01 +0300

    code reformat

--- includes/qmwp-search-correlations/qmwp-search-correlations-v1.php
+++ includes/qmwp-search-correlations/qmwp-search-correlations-v1.php
@@ -25,10 +25,12 @@ wp_enqueue_script("qmwp-search-correlations", plugins_url('../../', __FILE__) . 
 
                 </div>
                 <div>
-                    <input auto-complete ui-items="names"  ng-model="searchVariable" class="searchVariable" type="text" placeholder="Enter a medication, food supplement or anything else..." />
+                    <input auto-complete ui-items="names" ng-model="searchVariable" class="searchVariable" type="text"
+                           placeholder="Enter a medication, food supplement or anything else..."/>
                 </div>
                 <div>
-                    <input class="searchsubmit" type="button" value="Ask QuantiModo" ng-click="showCorrelations(searchVariable)" />
+                    <input class="searchsubmit" type="button" value="Ask QuantiModo"
+                           ng-click="showCorrelations(searchVariable)"/>
                 </div>
             </div>
         </div>
@@ -42,20 +44,26 @@ wp_enqueue_script("qmwp-search-correlations", plugins_url('../../', __FILE__) . 
                 <option value="effect">Predictive of...</option>
                 <option value="cause">Predicted by...</option>
             </select>
-            <input auto-complete ui-items="names" class="searchVariable" ng-model="searchVariable" type="text" placeholder="Enter a medication, food supplement or anything else...">
-            <input class="searchsubmit" type="button" value="Ask QuantiModo" ng-click="showCorrelations(searchVariable)">
+            <input auto-complete ui-items="names" class="searchVariable" ng-model="searchVariable" type="text"
+                   placeholder="Enter a medication, food supplement or anything else...">
+            <input class="searchsubmit" type="button" value="Ask QuantiModo"
+                   ng-click="showCorrelations(searchVariable)">
         </div>
 
         <p class="predictor">{{resultTitle}}</p>
+
         <p ng-show="isNotEmpty(correlations)">{{countAndTime}}</p>
 
         <div id="searchResultList">
             <ul>
                 <li ng-repeat="c in correlations">
-                    <div class="resultVariableName"><a href="http://www.amazon.com/gp/search/ref=as_li_qf_sp_sr_tl?ie=UTF8&camp=1789&creative=9325&index=aps&keywords={{c.variable}}&linkCode=ur2&tag=quant08-20" class="result-title" target="_blank">{{c.variable}}</a>
+                    <div class="resultVariableName"><a
+                            href="http://www.amazon.com/gp/search/ref=as_li_qf_sp_sr_tl?ie=UTF8&camp=1789&creative=9325&index=aps&keywords={{c.variable}}&linkCode=ur2&tag=quant08-20"
+                            class="result-title" target="_blank">{{c.variable}}</a>
                         <?php /*<span class="correlationValue">{{c.correlation}}</span> */ ?>
-                        <a href="http://www.amazon.com/gp/search/ref=as_li_qf_sp_sr_tl?ie=UTF8&camp=1789&creative=9325&index=aps&keywords={{c.variable}}&linkCode=ur2&tag=quant08-20" class="shop-cart" target="_blank">
-                            <img src="<?php echo plugins_url('../../','/css/images/shop-cart.png', __FILE__ );?>" />
+                        <a href="http://www.amazon.com/gp/search/ref=as_li_qf_sp_sr_tl?ie=UTF8&camp=1789&creative=9325&index=aps&keywords={{c.variable}}&linkCode=ur2&tag=quant08-20"
+                           class="shop-cart" target="_blank">
+                            <img src="<?php echo plugins_url('../../', '/css/images/shop-cart.png', __FILE__); ?>"/>
                         </a>
                     </div>
                     <div class="resultCategoryName">{{c.category}}</div>
@@ -63,7 +71,8 @@ wp_enqueue_script("qmwp-search-correlations", plugins_url('../../', __FILE__) . 
             </ul>
         </div>
         <div id="paginationSearchResultList" ng-show="hasMoreThanTen()">
-            <pagination total-items="bigTotalItems" ng-model="bigCurrentPage" max-size="maxSize" class="pagination-sm" ng-change="pageChanged()"></pagination>
+            <pagination total-items="bigTotalItems" ng-model="bigCurrentPage" max-size="maxSize" class="pagination-sm"
+                        ng-change="pageChanged()"></pagination>
         </div>
     </div>
 
