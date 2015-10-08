687a70b2d8b2c970434efd2e80871d07907552b6
vitality <vitaly.kiktiov@gmail.com>
2015-10-08 16:33:54 +0300

    responsive rating faces

--- includes/qm-rating-faces/qm-rating-faces-v1.php
+++ includes/qm-rating-faces/qm-rating-faces-v1.php
@@ -6,32 +6,58 @@ wp_enqueue_script("quantimodo-js-sdk", plugins_url('../../', __FILE__) . "js/lib
 wp_enqueue_script("quantimodo-intercom", plugins_url('../../', __FILE__) . "js/intercom.js", array('jquery', 'quantimodo-js-sdk'));
 ?>
 
-<div id="track-variable-content">
-    <!--    <div id="track-variable-header">How do you feel about <span id="track-variable-name"></span>?</div>-->
-    <section id="sectionRatingFaces">
-        <ul>
-            <li><img id="buttonFaceDepressed" class="track-icon" data-value="1"
-                     src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_depressed.png" ?>">
-                </img></li>
 
-            <li><img id="buttonFaceSad" class="track-icon" data-value="2"
-                     src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_sad.png" ?>">
-                </img></li>
+<div id="track-variable-content" class="shortcode-content rating-faces">
 
-            <li><img id="buttonFaceOk" class="track-icon" data-value="3"
-                     src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_ok.png" ?>">
-                </img></li>
+    <span>Please rate: <span id="tracked-variable-name"></span></span>
 
-            <li><img id="buttonFaceHappy" class="track-icon" data-value="4"
-                     src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_happy.png" ?>">
-                </img></li>
+    <div class="rating-buttons">
 
-            <li><img id="buttonFaceEcstatic" class="track-icon" data-value="5"
+        <div class="rating-button-wrap" data-value="1">
+            <div class="rating-button">
+                    <img id="buttonFaceDepressed" class="track-icon" data-value="1"
+                         src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_depressed.png" ?>">
+                    </img>
+            </div>
+        </div>
+
+        <div class="rating-button-wrap" data-value="2">
+            <div class="rating-button">
+                    <img id="buttonFaceSad" class="track-icon" data-value="2"
+                         src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_sad.png" ?>">
+                    </img>
+            </div>
+        </div>
+
+        <div class="rating-button-wrap" data-value="3">
+            <div class="rating-button">
+                    <img id="buttonFaceOk" class="track-icon" data-value="3"
+                         src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_ok.png" ?>">
+                    </img>
+            </div>
+        </div>
+
+        <div class="rating-button-wrap" data-value="4">
+            <div class="rating-button">
+                    <img id="buttonFaceHappy" class="track-icon" data-value="4"
+                         src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_happy.png" ?>">
+                    </img>
+            </div>
+        </div>
+
+        <div class="rating-button-wrap" data-value="5">
+            <div class="rating-button">
+                <img id="buttonFaceEcstatic" class="track-icon" data-value="5"
                      src="<?php echo plugins_url('../../', __FILE__) . "images/ic_mood_ecstatic.png" ?>">
-                </img></li>
-        </ul>
-    </section>
-    <section id="sectionSendingRating">
-    </section>
+                </img>
+            </div>
+        </div>
+
+        <div class="clearfix"></div>
+
+    </div>
+
+    <span id="result-string"></span>
+
 </div>
 
