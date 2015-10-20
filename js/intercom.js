25af1d68bf01b2e68bd79415f94dc105d3c7712e
vitality <vitaly.kiktiov@gmail.com>
2015-10-20 16:04:35 +0300

    wp intercom plugin integration

--- js/intercom.js
+++ js/intercom.js
@@ -1,46 +1,13 @@
 jQuery(document).ready(function () {
 
     Quantimodo.getCurrentUser(function (user) {
-        console.debug(user);
 
-        window.intercomSettings = {
-            app_id: 'wsj1w4xj',
-            name: user.displayName,
-            email: user.email,
-            created_at: new Date(user.userRegistered) / 1000
-        };
-
-        (function () {
-            var w = window;
-            var ic = w.Intercom;
-            if (typeof ic === 'function') {
-                ic('reattach_activator');
-                ic('update', intercomSettings);
-            } else {
-                var d = document;
-                var i = function () {
-                    i.c(arguments);
-                };
-                i.q = [];
-                i.c = function (args) {
-                    i.q.push(args);
-                };
-                w.Intercom = i;
-                function l() {
-                    var s = d.createElement('script');
-                    s.type = 'text/javascript';
-                    s.async = true;
-                    s.src = 'https://widget.intercom.io/widget/wsj1w4xj';
-                    var x = d.getElementsByTagName('script')[0];
-                    x.parentNode.insertBefore(s, x);
-                }
-
-                l();
-
-            }
-        })();
+        if (user && typeof window.intercomSettings !== 'undefined') {
+            window.intercomSettings.name = user.displayName;
+            window.intercomSettings.email = user.email;
+            window.intercomSettings.created_at = new Date(user.userRegistered) / 1000;
+        }
 
     });
-
 
 });
