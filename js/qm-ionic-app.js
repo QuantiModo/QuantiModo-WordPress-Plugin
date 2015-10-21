d7a8e77a45971965885a54fb2ea31c1ce96926e1
vitality <vitaly.kiktiov@gmail.com>
2015-10-21 14:58:18 +0300

    hound fix

--- js/qm-ionic-app.js
+++ js/qm-ionic-app.js
@@ -2,8 +2,6 @@ jQuery(document).ready(function () {
 
     var appHolder = jQuery('#ionic-app-holder');
     var showHideButton = jQuery('#qm-ionic-app-show-hide');
-    var iFrame = jQuery('#ionic-app-holder iframe');
-
 
     setTimeout(function () {
         showHideButton.show();
