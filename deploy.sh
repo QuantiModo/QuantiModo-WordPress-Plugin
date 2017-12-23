6e5ac357f62a42a0ed91c7cf6c91a5be2d9e1b68
Mike P. Sinn <m@quantimodo.com>
2017-12-23 17:51:21 -0600

    sh ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@
 # main config
 PLUGINSLUG="quantimodo"
 CURRENTDIR=`pwd`
-MAINFILE="quantimodo.php" # this should be the name of your main php file in the wordpress plugin
+MAINFILE="$PLUGINSLUG.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/quantimodo/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="mikepsinn" # your svn username
 
 
