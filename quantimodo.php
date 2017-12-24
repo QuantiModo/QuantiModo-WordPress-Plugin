f2f77815e665cc1938d61f9f99292507cbbc7183
Mike P. Sinn <m@quantimodo.com>
2017-12-23 19:06:21 -0600

    0.5.4

--- quantimodo.php
+++ quantimodo.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: QuantiModo
- * Version: 0.5.3
+ * Version: 0.5.4
  * Description: Allow your users to record, aggregate, analyze and visualize their health and life-tracking data.
  * Author: QuantiModo
  * Author URI: https://quantimo.do
