c247f7e5c226f910ac083edb32adec2cb4f16255
Mike P. Sinn <m@quantimodo.com>
2023-08-17 17:50:42 -0500

    aider: Generated webpack.config.js file with entry points for index.jsx, qm_iframe/index.jsx, and qm_redirect/index.jsx, and configured output to the build directory.
    # Aider chat conversation:
    
    USER: wp-scripts build doesn't seem to generate anything in the build folder
    
    ASSISTANT: The `wp-scripts build` command is a part of the `@wordpress/scripts` package. It's a set of reusable scripts tailored for WordPress development.
    
    If the `wp-scripts build` command doesn't generate anything in the `build` folder, it could be due to several reasons:
    
    1. The `webpack.config.js` file is not properly configured. It should have the correct entry and output points. The entry point should be your main JavaScript file (usually `src/index.js`), and the output should be the `build` directory.
    
    2. There might be errors in your JavaScript files that prevent the build process from completing. Check the console output when you run the `wp-scripts build` command to see if there are any error messages.
    
    3. The `build` command might not be correctly defined in your `package.json` file. It should be something like `"build": "wp-scripts build"`.
    
    4. The `@wordpress/scripts` package might not be correctly installed. You can try reinstalling it with `npm install @wordpress/scripts --save-dev`.
    
    However, without access to your `webpack.config.js` and `package.json` files, I can't provide a more specific solution. If you want me to help you debug this issue, please add these files to the chat.
    
    USER: I added these *read-write* files: package.json
    
    ASSISTANT: Thank you for providing the `package.json` file. The `build` script is correctly defined as `"build": "wp-scripts build"`.
    
    However, the `wp-scripts build` command relies on a `webpack.config.js` file to know what to build and where to output the built files. This file is usually located in the root directory of your project.
    
    Could you please add the `webpack.config.js` file to the chat so that I can propose the necessary changes?
    
    USER: please generate webpack.config.js
    
    ASSISTANT: Sure, I can help you generate a basic `webpack.config.js` file. This file will configure webpack to transpile your JavaScript files using Babel and output the bundled file to the `build` directory.
    
    Here is the proposed content for the `webpack.config.js` file:
    
    ```javascript
    webpack.config.js
    <<<<<<< HEAD
    =======
    const defaultConfig = require("@wordpress/scripts/config/webpack.config");
    
    module.exports = {
        ...defaultConfig,
        entry: {
            index: './src/index.jsx',
            qm_iframe: './src/qm_iframe/index.jsx',
            qm_redirect: './src/qm_redirect/index.jsx'
        },
        output: {
            filename: '[name].js',
            path: __dirname + '/build'
        },
        module: {
            rules: [
                {
                    test: /\.jsx$/,
                    exclude: /node_modules/,
                    use: {
                        loader: 'babel-loader',
                        options: {
                            presets: ['@babel/preset-env', '@babel/preset-react']
                        }
                    }
                }
            ]
        },
        resolve: {
            extensions: ['.js', '.jsx']
        }
    };
    >>>>>>> updated
    ```
    
    This configuration file tells webpack to create separate bundles for `index.jsx`, `qm_iframe/index.jsx`, and `qm_redirect/index.jsx`. The bundled files will be output to the `build` directory. The `babel-loader` is used to transpile JSX into regular JavaScript that can be run in the browser. The `@babel/preset-env` and `@babel/preset-react` presets are used to ensure compatibility with a wide range of browsers and to enable JSX syntax, respectively.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+
+module.exports = {
+    ...defaultConfig,
+    entry: {
+        index: './src/index.jsx',
+        qm_iframe: './src/qm_iframe/index.jsx',
+        qm_redirect: './src/qm_redirect/index.jsx'
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/build'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
